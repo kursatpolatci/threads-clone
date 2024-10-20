47f3759c701d148598f24ad18c9ff510ef1c5666
Kürşat Polatcı <polatcikursat58@gmail.com>
2024-10-20 21:26:40 +0300

    Frontend-Backend installation and User Model was created

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+import express from "express"
+import dotenv from "dotenv"
+import cookieParser from "cookie-parser"
+import connectDB from "./db/connectDB.js"
+
+import userRoutes from "./routes/userRoutes.js"
+
+dotenv.config()
+
+const app = express()
+
+const PORT = process.env.PORT || 5000
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(cookieParser())
+
+app.use("/api/users", userRoutes)
+
+app.listen(PORT, () => {
+    console.log(`Server started at http://localhost:${PORT}`)
+    connectDB();
+})
